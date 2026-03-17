8dedf12da161ef7e74a7993168fe8b205ab55066
ayush-may <ayushsharma014may@gmail.com>
2026-03-17 12:59:52 +0530

    integrated vercel ai sdk here with ollama and groq ai.

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,22 +1,44 @@
+import { groq } from "@ai-sdk/groq";
+import { convertToModelMessages, streamText, UIMessage } from "ai"
+import { ollama } from "ollama-ai-provider-v2"
+
 export async function POST(req: Request) {
 
-    const { messages } = await req.json()
+    const { messages }: { messages: UIMessage[] } = await req.json()
 
-    const ollamaResponse = await fetch("http://localhost:11434/api/chat", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            model: "qwen2.5:3b",
-            messages: messages,
-            stream: true
-        })
+    const result = streamText({
+        // model: ollama("qwen2.5:3b"),
+        model: groq("llama-3.1-8b-instant"),
+        system: 'return data in markdown format!',
+        messages: await convertToModelMessages(messages)
     })
 
-    return new Response(ollamaResponse.body, {
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
+    return result.toUIMessageStreamResponse();
 }
+
+
+/**
+ * old code and working
+ */
+// export async function POST(req: Request) {
+
+//     const { messages } = await req.json()
+
+//     const ollamaResponse = await fetch("http://localhost:11434/api/chat", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({
+//             model: "qwen2.5:3b",
+//             messages: messages,
+//             stream: true
+//         })
+//     })
+
+//     return new Response(ollamaResponse.body, {
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     })
+// }
